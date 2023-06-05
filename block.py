1e6eeadfde87073e2810d359e733f09150e8a217
David Safro <david.github24@gmail.com>
2023-06-05 16:15:45 -0400

    proof of work

--- block.py
+++ block.py
@@ -1,4 +1,6 @@
 import hashlib
+import time
+
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -6,6 +8,7 @@ class Block:
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
+        self.nonce = 0
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
@@ -13,8 +16,15 @@ class Block:
         sha.update(str(self.index).encode('utf-8') +
                    str(self.timestamp).encode('utf-8') +
                    str(self.data).encode('utf-8') +
-                   str(self.previous_hash).encode('utf-8'))
+                   str(self.previous_hash).encode('utf-8') +
+                   str(self.nonce).encode('utf-8'))
         return sha.hexdigest()
 
+    def mine_block(self, difficulty):
+        target = "0" * difficulty
+        while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
     def __str__(self):
-        return f"Block #{self.index}\nTimestamp: {self.timestamp}\nData: {self.data}\nPrevious Hash: {self.previous_hash}\nHash: {self.hash}"
+        return f"Block #{self.index}\nTimestamp: {self.timestamp}\nData: {self.data}\nPrevious Hash: {self.previous_hash}\nNonce: {self.nonce}\nHash: {self.hash}"
--- chain.py
+++ chain.py
@@ -4,6 +4,7 @@ from block import Block
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
+        self.difficulty = 4
 
     def create_genesis_block(self):
         return Block(0, "01/01/2023", "Genesis Block", "0")
@@ -13,7 +14,7 @@ class Blockchain:
 
     def add_block(self, new_block):
         new_block.previous_hash = self.get_latest_block().hash
-        new_block.hash = new_block.calculate_hash()
+        new_block.mine_block(self.difficulty)
         self.chain.append(new_block)
 
     def is_chain_valid(self):
